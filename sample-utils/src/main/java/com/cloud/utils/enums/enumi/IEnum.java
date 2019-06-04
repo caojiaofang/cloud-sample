3114f1ddcfbe63555f449b31d5d14824e85aeb7a
lizhi <1170118771@qq.com>
2019-06-04 09:48:19 +0800

    提交工具类

---
+++ sample-utils/src/main/java/com/cloud/utils/enums/enumi/IEnum.java
@@ -0,0 +1,20 @@
+/*
+ * 文件名：IEnum.java
+ * 版权：Copyright 2012-2016 YLINK Tech. Co. Ltd. All Rights Reserved. 
+ * 描述： IEnum.java
+ * 修改人：lizhi 1708029
+ * 修改时间：2019年6月4日
+ * 修改内容：新增
+ */
+package com.cloud.utils.enums.enumi;
+
+/**
+ * @Title:  IEnum.java
+ * @Package: com.cloud.utils.enums.IEnum.java
+ * @Description: 
+ * @Company: ylink
+ * @author  lizhi 
+ * @date  2019年6月4日 上午9:46:43
+ */
+public interface IEnum<T> extends NameIEnum, ValueIEnum<T> {
+}
